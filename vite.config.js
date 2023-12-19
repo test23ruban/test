412841e6a31bd3423630f8dade8b6d302e4162fa
test23ruban <154344303+test23ruban@users.noreply.github.com>
2023-12-19 20:49:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['/src/main.jsx'],
+      external: ['src/main.jsx'],
     },
   },
 });
